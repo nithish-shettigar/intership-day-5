ef134578890a7bc6e202777afb947494e371fdae
nithish-shettigar <129670979+nithish-shettigar@users.noreply.github.com>
2023-10-15 09:32:56 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,51 @@
+//prompt("enter the name","");
+//console.log("this is console");
+//console.log(6+5);
+//document.write("<h1>javascript</h1>");
+
+//var num1=5;
+//var num1=6
+//num1=8
+//console.log(num1);
+//let num2=9
+//num2=11
+//console.log(num2);
+
+//const num3=10
+
+//console.log(num3);
+//let num1=5
+//let num2="5"
+//console.log(num1==num2);
+//console.log(num1===num2);
+
+// function mul(n1,n2,n3,n4)
+// {
+//     return n1 *n2*n3*n4;
+// }
+
+// document.write(mul(4,5,6,7));
+
+// function greet(){
+//     alert("hello everyone")
+// }
+
+
+// for (let num=10;num>=1;num--)
+// {
+//     console.log(num);
+// }
+// dom - Document Object mode
+// dom manipulation method
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName
+// document.querySelector()
+// document.querySelectorAll()
+
+
+//  let el=document.getElementById("head").innerHTML;
+//  console.log(el);
+
+let ele=document.querrySelector("#head").innerHTML;
+console.log(ele);
